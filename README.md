7b33e05468b4769987a6c115acf57bc90ce2c52b
Niklan <niklanrus@gmail.com>
2014-09-08 19:36:45 +0600

    Added more features. Noew simples select is work, but not saves values.

--- README.md
+++ README.md
@@ -6,3 +6,46 @@ jQuery parser for &lt;select> element, for further convertation into HTML-Select
 ```javascript
 $('select').selectonic();
 ```
+
+Options
+==========
+
+```javascript
+$('select').selectonic({
+    afterInit: alert('Hello World after init!')
+})
+```
+
+| Option        | Default           | Type      |  Description |
+| ------------- |:-----------------:|:---------:|:-------------|
+| afterInit     | `function(){}`    | function  | Callback function after plugin initialized.             |
+| style         | classic           | function  | Name of function which gives selectObject for further generating HTML from that. |
+
+
+API
+==========
+Selectonic provides API as well.
+
+For using API create Selectonic instance.
+
+For single `<select>` element.
+
+```javascript
+var selectonic = $('select').selectonic().data('api_selectonic');
+```
+
+If you have multiple `<select>` elements use this code.
+
+```javascript
+$('select').each(function() {
+    var selectonic = $('select').selectonic().data('api_selectonic');
+});
+```
+Then you can use API methods.
+
+F.e.
+
+```javascript
+var selectonic = $('select').selectonic().data('api_selectonic');
+selectonic.parseSelect($('select'));
+```
--- src/selectonic.js
+++ src/selectonic.js
@@ -1,11 +1,22 @@
 /**
  * Selectonic - parse <select> tag to object, then convert it to html.
+ *
+ * @TODO: select option window styles: min width as select, auto with.
+ * @TODO: Default label for select: label+selected value, selected value.
+ * @TODO: Select position for options: auto, left, bottom-left, bottom,
+ *        bottom-right, right, top-left, top, top-right.
  */
 ;
 (function ($, window, document, undefined) {
 
     var pluginName = 'selectonic',
         defaults = {
+
+            // Style output.
+            style: 'classic',
+
+            // Class name using for detecting HTML selects.
+            className: 'selectonic',
 
             // After Selectonic initialization.
             afterInit: function () {
@@ -42,7 +53,16 @@
 
     // Selectonic initialization.
     Selectonic.prototype.init = function (element) {
+
+        // Translate our select element into Object.
         var selectObject = this.parseSelect(element);
+
+        // Generate HTML for select, based on style.
+        var selectHTML = this.generateSelectHTML(selectObject);
+
+        // Build our HTML select.
+        this.build(selectHTML);
+
     };
 
     /**
@@ -66,7 +86,7 @@
                     label: $(this).attr('label'),
                     options: $(this).find('option').map(function () {
                         return {
-                            value: $(this).attr('value'),
+                            value: $(this).attr('value') ? $(this).attr('value') : $(this).html(),
                             data: $(this).html()
                         };
                     })
@@ -76,20 +96,135 @@
         else {
             selectElement['options'] = $(element).find('option').map(function () {
                 return {
-                    value: $(this).attr('value'),
+                    value: $(this).attr('value') ? $(this).attr('value') : $(this).html(),
                     data: $(this).html()
                 };
             })
         }
 
+        return selectElement;
     };
+
+    /**
+     * Generate HTML Select from the Object based on style.
+     * @param selectObject - Object of select element, prepared by parseSelect().
+     *
+     * @TODO: support for custom styles.
+     */
+    Selectonic.prototype.generateSelectHTML = function (selectObject) {
+
+        var styleFunction = this.options.style;
+
+        if (styleFunction == 'classic') {
+            return this.styleClassic(selectObject);
+        }
+
+    }
+
+    /**
+     * Default style of select.
+     * @param selectObject
+     */
+    Selectonic.prototype.styleClassic = function (selectObject) {
+
+        var resultHTML = '';
+
+        var className = this.options.className;
+
+        resultHTML += '<div class="' + className + '">';
+        resultHTML += '<div class="' + className + '-select">' + 'Select a value  ▾' + '</div>';
+        resultHTML += '<div class="' + className + '-options-wrapper">';
+
+
+        if (selectObject.hasGroups) {
+            // @TODO: when select has groups.
+        }
+        else {
+
+            selectObject.options.each(function () {
+
+                resultHTML += '<div class="' + className + '-options-option" data-value="' + this.value + '">';
+                resultHTML += this.data;
+                resultHTML += '</div>';
+
+            });
+
+        }
+
+        // .selectonic-options-wrapper
+        resultHTML += '</div>';
+
+        resultHTML += '</div>';
+
+        return resultHTML;
+
+    }
+
+    /**
+     * Build our html select.
+     * @param selectHTML
+     */
+    Selectonic.prototype.build = function (selectHTML) {
+
+        var element = this.element;
+        var className = this.options.className;
+        var self = this;
+
+        $(element).hide();
+        $(element).after(selectHTML);
+
+        // Bind click for our select.
+        $('.'+className+'-select').bind('click', function (e) {
+            self.selectClick($(this), e);
+        })
+
+        // Hide select options by default.
+        $('.'+className+'-options-wrapper', $('.'+className+'-select').parent()).hide();
+
+    }
+
+    /**
+     * Function for handling selectonic click.
+     * @param event
+     */
+    Selectonic.prototype.selectClick = function (element, event) {
+
+        var className = this.options.className;
+
+        // .selectonic-select
+        var selectElement = $(element).get(0);
+
+        // @TODO: Open\close select div.
+        if ($(selectElement).hasClass('opened')) {
+
+            $(selectElement).toggleClass('opened');
+            $('.'+className+'-options-wrapper', $(selectElement).parent()).hide();
+
+        }
+        else {
+
+            $(selectElement).toggleClass('opened');
+            $('.'+className+'-options-wrapper', $(selectElement).parent()).show();
+
+            // Setting up position for options.
+            var position = $(selectElement).parent().position();
+            var height = $(selectElement).outerHeight(true);
+
+            $('.'+className+'-options-wrapper', $(selectElement).parent()).css({
+                position: 'absolute',
+                top: position.top + height,
+                left: position.left
+            });
+        }
+
+    }
 
     // Plugin registration.
     $.fn[pluginName] = function (options) {
 
         return this.each(function () {
             if (!$.data(this, 'api_' + pluginName)) {
-                var instance = $.data(this, 'api_' + pluginName,
+                $.data(this, 'api_' + pluginName,
                     new Selectonic($(this), options));
             }
         });
